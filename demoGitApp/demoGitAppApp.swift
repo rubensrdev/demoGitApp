585f42ed847dc156371b2c4ea7de642631157e68
Rubén <ruben.sr.apple@gmail.com>
2024-10-18 20:10:39 +0200

    Initial Commit

---
+++ demoGitApp/demoGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGitAppApp.swift
+//  demoGitApp
+//
+//  Created by Rubén Segura Romo on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct demoGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demoGitAppTests/demoGitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  demoGitAppTests.swift
+//  demoGitAppTests
+//
+//  Created by Rubén Segura Romo on 18/10/24.
+//
+
+import Testing
+@testable import demoGitApp
+
+struct demoGitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
