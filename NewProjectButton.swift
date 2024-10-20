1315c81cea8d09bf83c8094618ce569b8ee60d9d
Rubén <ruben.sr.apple@gmail.com>
2024-10-20 13:09:42 +0200

    new button to start project and new view

---
+++ NewProjectButton.swift
@@ -0,0 +1,18 @@
+//
+//  NewProjectButton.swift
+//  demoGitApp
+//
+//  Created by Rubén Segura Romo on 20/10/24.
+//
+
+import SwiftUI
+
+struct NewProjectButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewProjectButton()
+}
